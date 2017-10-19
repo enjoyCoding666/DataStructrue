1396c830557aee435be1fd597c6f5db0de8407b6
expiator <huaiyinnianshao28@163.com>
2017-10-19 22:57:26 +0800

    添加Node、链表的操作

---
+++ src/main/java/linkList/LinkListOperation.java
@@ -0,0 +1,23 @@
+package linkList;
+
+/**
+ *  @author lin
+ *
+ **/
+public class LinkListOperation {
+    public void findNode(){
+
+    }
+
+    public void insertNode(){
+
+    }
+
+    public void updateNode(){
+
+    }
+
+    public void removeNode(){
+
+    }
+}
---
+++ src/main/java/linkList/Node.java
@@ -0,0 +1,9 @@
+package linkList;
+
+public class Node {
+    Node next;
+    Object object;
+    public Node (Object object){
+        this.object=object;
+    }
+}
